5c1879e7564e64410da3cf4807249282d9250880
lomteankita <aankitalomte@gmail.com>
2023-01-21 22:25:15 +0530

    day-5

--- shopping/src/Pages/Jeans.jsx
+++ shopping/src/Pages/Jeans.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import {useState,useEffect} from "react";
 
 const Jeans = () => {
+  const[adata ,setData]=useState([]);
+
+  let getdata=async()=>{
+    return fetch (`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/getrestaurants?limit=20`)
+    .then((res)=> res.json())
+   }
+   let fetchdata=async()=>{
+    
+    let data=await getdata();
+    
+    setData(data.data)
+    console.log(data.data)
+   
+   }
+   useEffect(()=>{
+    fetchdata();
+   },[]);
+
+
+
   return (
-    <div>Jeans</div>
+    <div>
+
+<h1 >Restaurants List</h1>
+<div  style={{height:"500px",margin:"auto",display:"grid", gridTemplateColumns:" 1fr 1fr 1fr 1fr",
+         width:"90%"}}>
+       {/* Restaurant Card */}
+       { adata.map((e)=>(
+        <div >
+          <img src={e.image} />
+          <h1> {e.name}</h1>
+         <h2> RS : {e.price_starts_from}</h2>
+         
+         <button> ADD TO CARD</button>
+         <br />
+         <br />
+         <br />
+          </div >
+         
+       ))}
+      </div>
+
+
+
+
+
+    </div>
   )
 }
 
